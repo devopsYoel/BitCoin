fb080048e2f30ef1d346e01317c489503d19a8e6
Yoel <yoellevy93@gmail.com>
2018-08-05 17:23:23 +0200

    Add project files.

---
+++ BitCoin/BitCoin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitCoin
+{
+    class Bitcoin
+    {    
+        public Time time { get; set; }
+        public string disclaimer { get; set; }
+        public string chartName { get; set; }
+        public BPI bpi { get; set; }
+        
+    }
+
+    class Time
+    {
+        public string updated { get; set; }
+        public string updatedISO { get; set; }
+        public string updateduk { get; set; }
+    }
+
+    class BPI
+    {
+        public USD usd { get; set; }      
+    }
+
+    class USD
+    {
+        public string code { get; set; }
+        public string symbol { get; set; }
+        public string rate { get; set; }
+        public string description { get; set; }
+        public string rate_float { get; set; }
+    }
+
+
+
+}
---
+++ BitCoin/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Globalization;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Flurl.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BitCoin
+{
+    class Program
+    {    
+        static void Main()
+        {
+            int count = 0;
+            float valueOdd = 0;
+            using (var client = new WebClient())
+            {
+                while(true)
+                {
+
+                    string response = client.DownloadString("https://api.coindesk.com/v1/bpi/currentprice.json");
+
+                    Bitcoin m = JsonConvert.DeserializeObject<Bitcoin>(response);
+
+                    count++;
+
+                    string value = m.bpi.usd.rate_float;
+                    float valueNew = float.Parse(value, CultureInfo.InvariantCulture.NumberFormat);
+                    float average = (valueNew + valueOdd*(count-1)) / count;
+                   
+
+                    Console.WriteLine(average);
+                    
+                    
+                    valueOdd = valueNew;
+                   
+                    // sleep 5 sec and call api again
+                    System.Threading.Thread.Sleep(5000);
+                }
+
+            }
+
+
+            
+        }
+    }       
+      
+}
